c1fa279e5cac496d526f3af85bf9c7901723fc54
pp <18351585965@163.com>
2025-09-23 13:50:59 +0800

    update

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
-import "github.com/xuepp/godemo/pkg/functions"
+import "github.com/xuepp/godemo/packages/cli"
 
 //import "github.com/xuepp/godemo/packages/cli"
 
 func main() {
 	//functions.FunctionDemo()
-	functions.MethodDemo()
+	//functions.MethodDemo()
 	//types.StructDemo()
 	//types.ArrayDemo()
 	//types.SliceDemo()
 	//types.InterfaceDemo()
 	//cli.Greet()
 	//cli.Flags()
-	//cli.Demo()
+	cli.Demo()
+
 }
 
 // 定义一个打印器
